18eb7e7af4d7e8125c9dd293727e60f733a8c190
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-07-04 07:03:45 -0700

    in 3-get_ids_sum.js requered files added

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getListStudentIds(arr) {
-  return array.reduce((total, student) => total + student.id, 0);
+  return arr.reduce((total, student) => total + student.id, 0);
 }
